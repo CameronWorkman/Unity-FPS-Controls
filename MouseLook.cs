7ea7f1d4350e22e7457837c3de05b65b8c30bb43
Cameron Workman <workmancam@gmail.com>
2020-07-29 15:57:18 -0700

    Add files via upload

---
+++ MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    private void Update()
+    {
+        // sets mouseX and mouseY to raw mouse controller movements. multiples by sensitivity to allow control.
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        //rotates camera in the Y axis based on mouse movements, clamped to prevent player from looking past 90 degrees above or below themselves.
+        //slippy was like "do a barrel roll" and I said NO I WILL NOT.
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotates the parent transform object in the X axis based on mouse movements. allows control of player object based on directional view.
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController playerController;
+
+    public float speed;
+    public float sprintSpeedMultiplier;
+    public float gravity;
+    public float jumpHeight;
+    private bool doubleJump;
+
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isTouchingGround;
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //this controls movement in the x and z directions based on input
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        //instantiates a sphere at a given location using an empty object as a marker, tests if the sphere is touching anything tagged ground.
+        //adjust bool to true if yes, false if no.
+        isTouchingGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        //noticed gravity acted odd if you didn't have a constant low velocity even when touching ground.
+        if (isTouchingGround && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+        //multiplies speed by a public float sprintSpeedMultiplier when LeftShift is held down.
+        //if it isn't, applies normal move * speed to the character controller.
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            playerController.Move(move * speed * sprintSpeedMultiplier * Time.deltaTime);
+        }
+        else
+        {
+            playerController.Move(move * speed * Time.deltaTime);
+        }
+
+        //when Jump is pressed, jumps if touching ground. if yes, jumps and sets doubleJump bool to True. If no, checks if doubleJump is True and jumps.
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isTouchingGround)
+            {
+                velocity.y = 0;
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                doubleJump = true;
+            } else if (doubleJump)
+            {
+                velocity.y = 0;
+                velocity.y = Mathf.Sqrt(jumpHeight * -3f * gravity);
+                doubleJump = false;
+            }
+        }
+
+
+        //applies gravity to the player at all times. momentum builds up even if touching the ground, hence the earlier velocity adjustment if touching ground.
+        velocity.y += gravity * Time.deltaTime;
+        playerController.Move(velocity * Time.deltaTime);
+    }
+}
